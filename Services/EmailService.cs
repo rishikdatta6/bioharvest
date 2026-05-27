6a43fe8230f429954ddeb169d4d5358804e44b04
rishikdatta6 <rishikdatta6@gmail.com>
2026-05-28 04:08:37 +0530

    SMTP debugging

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -10,23 +10,47 @@ public class EmailService : IEmailService
 {
     public async Task SendEmail(string to, string subject, string body)
     {
+        Console.WriteLine("STEP 1");
+
         var smtp = new SmtpClient("smtp.gmail.com")
         {
             Port = 587,
-            Credentials = new NetworkCredential("rishikdatta6@gmail.com", "erlxnpgdayplueif"),
+
+            Credentials = new NetworkCredential(
+                "rishikdatta6@gmail.com",
+                "erlxnpgdayplueif"
+            ),
+
             EnableSsl = true,
-            UseDefaultCredentials=false
+            UseDefaultCredentials = false,
+
+            Timeout = 10000
         };
 
-        var message = new MailMessage("rishikdatta6@gmail.com", to, subject, body);
+        Console.WriteLine("STEP 2");
+
+        var message = new MailMessage(
+            "rishikdatta6@gmail.com",
+            to,
+            subject,
+            body
+        );
+
+        Console.WriteLine("STEP 3");
+
         try
         {
             await smtp.SendMailAsync(message);
-            Console.WriteLine("✅ SENT");
+
+            Console.WriteLine("STEP 4");
+            Console.WriteLine("EMAIL SENT");
         }
         catch (Exception ex)
         {
-            Console.WriteLine("EMAIL ERROR: " + ex.ToString());
+            Console.WriteLine("EMAIL ERROR:");
+            Console.WriteLine(ex.ToString());
+
+            throw;
         }
     }
 }
