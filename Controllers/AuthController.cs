27d74943cba8a3e5160f2d7b0da91c3a017d75cc
rishikdatta6 <rishikdatta6@gmail.com>
2026-05-28 22:30:02 +0530

    Newest change

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -21,24 +21,7 @@ namespace BioHarvest.Api.Controllers
             _userManager = userManager;
             _emailService = emailService;
         }
-      
-        //[HttpPost("register")]
-        //public async Task<IActionResult> Register([FromBody] RegisterModel model)
-        //{
-        //    var user = new AppUser
-        //    {
-        //        UserName = model.Email,
-        //        Email = model.Email,
-        //        Name = model.Name
-        //    };
 
-        //    var result = await _userManager.CreateAsync(user, model.Password);
-
-        //    if (!result.Succeeded)
-        //        return BadRequest(result.Errors);
-
-        //    return Ok(new { message = "User registered" });
-        //}
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
